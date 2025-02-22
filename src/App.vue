f4c8739a46b1ad0726f21df8383b728670fb8298
ShahinAlam7893 <shahinalam7893@gmail.com>
2025-02-22 23:44:09 +0600

    Ostad live test

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import { defineProps}from 'vue';
+
+const myChoice = {Rock, Paper, Scissor};
+const computerChoice = {Rock, Paper, Scissor};
+
+
+
+      computerChoice = Math.floor(Math.random()*3);
+
+      if(computerChoice === myChoice){
+        return "It's a tie match!";
+      }
+      else if(computerChoice === Rock && myChoice === Scissor){
+        return "Computer win";
+      }else if(computerChoice === Scissor && myChoice === Paper){
+        return "Computer win";
+      }else if(computerChoice === Paper && myChoice === Rock){
+        return "Computer win";
+      }else{
+        return "You win";
+      }
+
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+</style>
